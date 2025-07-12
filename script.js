fc90023c10e5bda375f1cae57123cba8a527173d
Lucca Trevisan <141439341+luccatrevisan@users.noreply.github.com>
2025-07-12 11:20:04 -0300

    html changes & some js lines

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-/*
-preciso de ciclos e horas
+let ciclo = document.getElementById('ciclo');
+let hora = document.getElementById('hora');
+let tempoRestante = document.getElementsByClassName('tempoRestante');
 
-*/
+function startPomodoro(){
+    /*
+    ao clicar:
+        pega o valor de ciclo ou de hora
+            ignora hora por enquanto. foca no ciclo
+        com esse valor, determina o tempo total
+    */
+    tempoRestante = (Number(ciclo.value) * 30) / 60 //em horas
+    console.log(tempoRestante)
+}
+
+function selectHeader(){
+    /*
+    display none vem por padrão
+    se clicar no label, display select
+    
+    */
+}
